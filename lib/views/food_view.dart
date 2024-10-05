65438f1ae2c60fcba7010a6494a510a673741476
mohamed <mohamedomer2582004@gmail.com>
2024-10-05 12:10:03 +0300

    category food done

--- lib/views/food_view.dart
+++ lib/views/food_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/constants.dart';
-import 'package:recipe_app/widgets/category_food.dart';
+import 'package:recipe_app/widgets/category_food_list_view.dart';
 import 'package:recipe_app/widgets/category_list_view.dart';
 import 'package:recipe_app/widgets/custom_app_bar.dart';
 import 'package:recipe_app/widgets/custom_search_widget.dart';
@@ -15,24 +15,30 @@ class FoodView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 30),
-          const CustomAppBar(),
-          const SizedBox(height: 10),
-          CustomSearchWidget(onTap: () {}),
-          const SizedBox(height: 20),
-          const CustomText(text: "Trending Today"),
-          const SizedBox(height: 20),
-          const CustomTrendingFoodListWidget(),
-          const SizedBox(height: 20),
-          const CustomText(text: "Categories"),
-          const SizedBox(height: 20),
-          const CategoryListView(),
-          const SizedBox(height: 80),
-          CategoryFood(onTap: () {}, foodName: 'Salad 2.0'),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+            const CustomAppBar(),
+            const SizedBox(height: 10),
+            CustomSearchWidget(onTap: () {}),
+            const SizedBox(height: 20),
+            const CustomText(text: "Trending Today"),
+            const SizedBox(height: 20),
+            const CustomTrendingFoodListWidget(),
+            const SizedBox(height: 20),
+            const CustomText(text: "Categories"),
+            const SizedBox(height: 20),
+            const CategoryListView(),
+            const SizedBox(height: 100),
+            const CategoryFoodListView(),
+            const SizedBox(height: 20),
+            const CustomText(text: "Verfied Chefs"),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/category_food.dart
+++ lib/widgets/category_food.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/widgets/custom_category_food_body.dart';
+import 'package:recipe_app/widgets/custom_category_food_title.dart';
 
 class CategoryFood extends StatelessWidget {
   final VoidCallback onTap;
@@ -17,116 +19,15 @@ class CategoryFood extends StatelessWidget {
       child: Stack(
         clipBehavior: Clip.none,
         children: [
-          Card(
-            child: Container(
-              height: 160,
-              width: 140,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(22),
-                gradient: const LinearGradient(
-                  colors: [
-                    Color(0XFFF99FAF),
-                    Color(0XFFF8C39D),
-                  ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  stops: [0.2, 0.8],
-                ),
-              ),
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.hourglass_full_outlined,
-                        color: Color(0XFF655651),
-                      ),
-                      Text(
-                        "10-20mins",
-                        style: TextStyle(
-                          color: Color(0XFF655651),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 5),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.group,
-                        color: Color(0XFF655651),
-                      ),
-                      Text(
-                        "4-5People",
-                        style: TextStyle(
-                          color: Color(0XFF655651),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 5),
-                  Row(
-                    children: [
-                      Spacer(),
-                      Text(
-                        "Maria",
-                        style: TextStyle(
-                          color: Color(0XFF655651),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Icon(
-                        Icons.person_2,
-                        color: Color(0XFF655651),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+          const CustomCategoryFoodBody(),
+          const Positioned(
+            left: 16,
+            top: 138,
+            child: CustomCategoryFoodTitle(foodName: "Salad 2.0"),
           ),
           Positioned(
-              left: 16,
-              top: 130,
-              child: Stack(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(left: 6),
-                    padding: const EdgeInsets.only(left: 18),
-                    decoration: BoxDecoration(
-                      color: const Color(0XFF103C4A),
-                      borderRadius: BorderRadius.circular(6),
-                    ),
-                    alignment: Alignment.centerLeft,
-                    width: 120,
-                    height: 40,
-                    child: Text(
-                      foodName,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                  const Positioned(
-                    top: 10,
-                    left: 0,
-                    bottom: 10,
-                    child: Icon(
-                      Icons.circle,
-                      color: Colors.green,
-                      size: 16,
-                    ),
-                  ),
-                ],
-              )),
-          Positioned(
             left: 4,
-            top: -63,
+            bottom: 128,
             child: CircleAvatar(
               radius: 70,
               child: Container(),
